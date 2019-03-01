7046056ec77657246904b265a32072263a52df01
Orfeas Kalipolitis <orfeaskalipolitis@gmail.com>
2019-03-01 21:10:13 +0200

    Added use of distance router

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+//  Import standard modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//  Import routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var distanceRouter = require('./routes/distance');
 
 var app = express();
 
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/distance', distanceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
